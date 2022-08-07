15bb1644883acf80a72eaa1942e07c1663d44237
정민식 <jms6025a@naver.com>
2022-08-07 14:38:42 +0900

    코딩테스트 기초

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("hello world")
-print("hello starbucks")
+numbers = {
+    "zero" : 0,
+    "one" : 1,
+    "two" : 2,
+    "three" : 3,
+    "four" : 4,
+    "five" : 5,
+    "six" : 6,
+    "seven" : 7,
+    "eight" : 8,
+    "nine" : 9
+}
+
+def solution(s: str):
+    for k, v in numbers.items():
+        s = s.replace(k, str(v))
+    return int(s)
+
+
+rv = solution("one4seveneight")
+print(rv)
